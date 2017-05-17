e0889107b3c162de18648dd7f0bbcd363ee15b49
Hack Reactor Student <student@hackreactor.com>
2017-05-17 14:54:05 -0700

    Completed functional pattern tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    if (counter > 0) {
+      var dequeue = storage[0];
+      counter--;
+      for (var i = 0; i < counter; i++) {
+        storage[i] = storage[i + 1];
+      }
+
+      delete storage[counter];
+      return dequeue;
+    } 
+    
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
