46dd92ce4c0e58d2ee8ea42076f0a42fec010d98
Hack Reactor Student <student@hackreactor.com>
2017-05-18 20:45:56 -0700

    completed graph functions and implementation

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -22,6 +22,15 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  var existingEdges = this.edges[node];
+
+
+  // call removeEdge for every possible edge within the edges array
+  for (let i = 0; i < existingEdges.length; i++) {
+    this.removeEdge(node, existingEdges[i]);
+  }
+
+  // removes node and also the edges for that node
   this.vertices.splice(this.vertices.indexOf(node), 1);
   this.edges.splice(node, 1);
   
@@ -29,6 +38,11 @@ Graph.prototype.removeNode = function(node) {
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // if a node doesn't exist to begin with, return false immediately
+  if (this.edges[toNode] === undefined && this.edges[fromNode]) {
+    return false;
+  }
+
   // initialize two flags
   var flag1 = false;
   var flag2 = false;
@@ -64,11 +78,44 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  
+  // array deletion 
+  // check toNode for the edge and delete it if its found
+    // look for fromNode in edge.toNode array
+  for (let i = 0; i < this.edges[toNode].length; i++) {
+    if (this.edges[toNode][i] === fromNode) {
+      // found node! if its the last object, you just pop your array, else you
+      if (i === this.edges[toNode].length - 1) {
+        this.edges[toNode].pop();
+      } else {
+      // you gotta shift everything down AFTER node finding
+        for (let j = i; j < this.edges[toNode].length - 1; j++) {
+          this.edges[toNode][j] = this.edges[toNode][j + 1];
+        }
+      }
+    }
+  }
+  // check fromNode for the edge and delete if its found
+  for (let i = 0; i < this.edges[fromNode].length; i++) {
+    if (this.edges[fromNode][i] === toNode) {
+      // found node! if its the last object, you just pop your array, else you
+      if (i === this.edges[fromNode].length - 1) {
+        this.edges[fromNode].pop();
+      } else {
+      // you gotta shift everything down AFTER node finding
+        for (let j = i; j < this.edges[fromNode].length - 1; j++) {
+          this.edges[fromNode][j] = this.edges[fromNode][j + 1];
+        }
+      }
+    }
+  }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let i = 0; i < this.vertices.length; i++) {
+    cb(this.vertices[i]);
+  }
 };
 
 /*
