1b99485617455afb571d42978eca352e974941fd
Hack Reactor Student <student@hackreactor.com>
2017-05-19 08:47:31 -0700

    added set functionality

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = 1;
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage[item]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item]) {
+    delete this._storage[item];
+  }
 };
 
 /*
