e7fbed2c142959033aaec4a6fa5d911f7815cd1b
Hack Reactor Student <student@hackreactor.com>
2017-05-19 13:19:46 -0700

    completed hash table implementation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,61 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // get LimitedArray index... var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index);
+  console.log(bucket);
+  if (bucket === undefined) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    var duplicateFound = false;
+    // what if there's a duplicate?
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        // this basically overwrites a discovered key's value with the new value
+        bucket[i][1] = v;
+        duplicateFound = true;
+      }
+      if (!duplicateFound) {
+        bucket.push([k, v]);
+      }
+      
+      this._storage.set(index, bucket);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index); 
+
+  if (bucket !== undefined) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var bucketLength = bucket.length;
+
+  if (bucket !== undefined) {
+    for (let i = 0; i < bucketLength; i++) {
+      if (bucket[i][0] === k) {
+        delete bucket[i];
+        if (bucket.length === 1) {
+          bucket = undefined;
+        }
+      }
+    }
+  }
+
+  this._storage.set(index, bucket);
 };
 
 
